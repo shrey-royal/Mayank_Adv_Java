c114477f4d356119c04cc5290018033cc310651a
Shrey K <srkkadia@gmail.com>
2023-07-31 15:04:05 +0530

    MVC

---
+++ MVC_Struct/src/com/util/DBConnection.java
@@ -0,0 +1,45 @@
+package com.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	public static Connection getConnection() {
+		Connection conn = null;
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/ecommerce";
+		String userName = "root";
+		String password = "root";
+		
+		try {
+			//load driver
+			Class.forName(driver);
+			
+			//jdbc api open connection
+			conn = DriverManager.getConnection(url, userName, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	public static void disconnect(Connection conn) {
+		try {
+			if(conn != null || !conn.isClosed()) {
+				conn.close();
+				System.out.println("DB Connection closed successfully...");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+//	public static void main(String[] args) {
+//		Connection conn = DBConnection.getConnection();
+//		System.out.println(conn);
+//	}
+}
